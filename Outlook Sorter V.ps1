a47eea49539d87ac5ebfc4786bd6bc69ebf892ef
Michael Vabner <michaelvabner@gmail.com>
2016-08-08 11:49:08 -0400

    Add files via upload

--- Outlook Sorter V.ps1
+++ Outlook Sorter V.ps1
@@ -1,1 +1,20 @@
-﻿
+﻿#Function to look through excel file and turn contents of first column into list 
+$Excel = New-Object -ComObject Excel.Application 
+$Excel.Visible = $true
+$Excel.DisplayAlerts = $false
+$ExcelWorkBook = $Excel.Workbooks.Open("C:\Users\michael.vabner\Documents\Book2.xlsx") 
+$ExcelWorkSheet = $Excel.Sheets.item("Sheet1") 
+$ExcelWorkSheet.activate() 
+$arrBlackListEmails = @()
+$i = 1
+$string = $ExcelWorkSheet.Cells.Item($i, 1).Value()
+Do 
+{
+    $arrBlackListEmails += $ExcelWorkSheet.Cells.Item($i, 1).Value()
+    $i = $i + 1
+}
+Until ($ExcelWorkSheet.Cells.Item($i, 1).Value() -eq $null)
+
+
+
+
